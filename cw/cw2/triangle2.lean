13513f5574d99cea6306837ac94a96fc397ff224
leorjones <leorileyjones@gmail.com>
2024-02-29 14:45:09 +0000

    nruhrhf

--- cw/cw2/triangle2.lean
+++ cw/cw2/triangle2.lean
@@ -28,3 +28,52 @@ instance : Group (MyD₃) where
   mul_one := by sorry
   inv := D₃inv
   mul_left_inv := by sorry
+
+/- Prove any theorems about the definitions of multiplication, idetities and inverses and so on here.
+such as
+theorem r_mul_r (i j : ZMod n) : r i * r j = r (i + j) := rfl
+ Also results about cardinality perhaps
+
+ theorem card [NeZero n] : Fintype.card (DihedralGroup n) = 2 * n := by
+  rw [← Fintype.card_eq.mpr ⟨fintypeHelper⟩, Fintype.card_sum, ZMod.card, two_mul]
+#align dihedral_group.card DihedralGroup.card
+
+theorem nat_card : Nat.card (DihedralGroup n) = 2 * n := by
+  cases n
+  · rw [Nat.card_eq_zero_of_infinite]
+  · rw [Nat.card_eq_fintype_card, card]
+   -/
+
+end MyD₃
+
+variable (n : ℕ)
+
+def n_set : Set ℕ := Finset.range n
+
+variable (a1 a2 a3 b1 b2 b3 c1 : n_set n)
+
+def n7 := n_set (n) × n_set (n) × n_set (n) × n_set (n) ×  n_set (n) × n_set (n) × n_set (n)
+
+def example_element : n7 n := (a1, a2, a3, b1, b2, b3, c1)
+
+def rotation_funct : n7 n → n7 n
+| ⟨a1, a2, a3, b1, b2, b3, c1 ⟩ => (a2, a3, a1, b2, b3, b1, c1)
+
+def reflection_0_funct : n7 n → n7 n
+| ⟨a1, a2, a3, b1, b2, b3, c1 ⟩ => (a1, a3, a2, b3, b2, b1, c1)
+
+def reflection_1_funct : n7 n → n7 n
+| ⟨a1, a2, a3, b1, b2, b3, c1 ⟩ => (a2, a1, a3, b1, b3, b2, c1)
+
+def reflection_2_funct : n7 n → n7 n
+| ⟨a1, a2, a3, b1, b2, b3, c1 ⟩ => (a3, a2, a1, b2, b1, b3, c1)
+
+open MyD₃
+
+def myaction2: MyD₃ → (n7 n → n7 n)
+| r 0 => id
+| r 1 => rotation_funct n
+| r 2 => (rotation_funct n) ∘ (rotation_funct n)
+| sr 0 => reflection_0_funct n
+| sr 1 => reflection_1_funct n
+| sr 2 => reflection_2_funct n
