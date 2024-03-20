38d491fd801470df8e0617270f87a93f4ad1d9f5
Jones <lrj21@ic.ac.uk>
2024-03-20 13:03:08 +0000

    qi hate lean

--- cw/cw3/draft.lean
+++ cw/cw3/draft.lean
@@ -3,7 +3,7 @@ import Mathlib
 /-# Sylow Theorems yay-/
 
 /- # Preliminaries -/
-variable (p : ℕ) (G H : Type*) [Group G] {H : Subgroup G} {pCond : p.Prime}
+variable (p : ℕ) (G H : Type*) [Group G] {H : Subgroup G} {pCond : Fact p.Prime}
 variable {p} {G}
 
 open BigOperators MulAction
@@ -37,7 +37,7 @@ nₚ(G) ≡ 1 % p -/
 
 local notation "Φ" => Quotient <| orbitRel G (Sylow p G)
 
-lemma orbit_div_G [Fintype G] [∀ x : X, Fintype (orbit G x)] : Fintype.card (orbit G x) ∣ Fintype.card G  := by sorry
+lemma orbit_div_G [Fintype G] [∀ x : X, Fintype (orbit G x)] (y : X): Fintype.card (orbit G y) ∣ Fintype.card G  := by sorry
 
 
 -- lemma orbit_one : :=
@@ -48,14 +48,30 @@ lemma orbit_div_G [Fintype G] [∀ x : X, Fintype (orbit G x)] : Fintype.card (o
 --     infer_instance
 --   have h2 : Fintype.card (fixedPoints P (Sylow p G)) = 1 := by simp [h]
 
-theorem SylowII [Fintype (Sylow p G)]: Fintype.card (Sylow p G) ≡ 1 [MOD p] := by
-  have P : Sylow p G := by sorry
+open Fintype
+
+theorem SylowII [Fintype (Sylow p G)](P : Sylow p G)(y : Sylow p G)[∀ x : Sylow p G, Fintype (orbit P x)]: Fintype.card (Sylow p G) ≡ 1 [MOD p] := by
+  -- have P : Sylow p G := by sorry
   have h : fixedPoints P (Sylow p G) = {P} := by sorry
   have _ : Fintype (fixedPoints P (Sylow p G)) := by
     rw[h]
     infer_instance
   have h2 : Fintype.card (fixedPoints P (Sylow p G)) = 1 := by simp [h]
-  have h3 (Q : Sylow p G)[Fintype (fixedPoints Q (Sylow p G))]: Fintype.card (fixedPoints Q (Sylow p G)) = 1 → P = Q := by sorry
+  have h3 : Fintype.card (orbit P y) = 1 → (orbit P y) = {P} := by sorry
+  have h4 [Fintype P]: Fintype.card (orbit P y) ∣  Fintype.card P := by
+    apply orbit_div_G
+  have h5 [Fintype P][Fintype G] : p ∣ Fintype.card P := by
+    simp[Nat.instDvdNat]
+    obtain ⟨n, heq : card P = _⟩ := IsPGroup.iff_card.mp P.isPGroup'
+    -- cases n
+    -- rw [heq]
+    -- simp
+    use p ^ (n-1)
+    rw[heq]
+    ring_nf
+    sorry
+  have h6 : Fintype.card (orbit P y) ≠ 1 → p ∣ Fintype.card (orbit P y) := by sorry
+
   sorry
 
 /- # Sylow III
